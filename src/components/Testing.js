7eae6b37a4866866300f2cb37bd410e918baed7a
AlwaysFocusTest <81003994+AlwaysFocusTest@users.noreply.github.com>
2021-03-21 16:28:44 -0600

    First Commit

---
+++ src/components/Testing.js
@@ -0,0 +1,11 @@
+const [project, setProject] = useState({
+    projName: "",
+    projType: "",
+    projLanguage: "",
+    projPlatform: "",
+  });
+  const [projectName, setProjectName] = useState("");
+  const [projectType, setProjectType] = useState("");
+  const [projectLanguage, setProjectLanguage] = useState("");
+  const [projectPlatform, setProjectPlatform] = useState("");
+  const [step, setStep] = useState(0);
